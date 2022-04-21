5681ff28e01311293ceebf1d8264d9e840f6bc4a
Timothydavid Maciel <timothydavidmaciel@gmail.com>
2022-04-21 16:23:42 -0500

    eed to restart mac

---
+++ js/for_loops.js
@@ -0,0 +1,22 @@
+// function showMultiplicationTable(num) {
+//     for (let i = 1; i < 11; i++) {
+//         console.log(num * i);
+//     }
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     let random = Math.floor((Math.random() * 181) + 20)
+//     if (random % 2 === 0) {
+//         console.log(random + " is even.");
+//     } else {
+//         console.log(random + " is odd.");
+//     }
+// }
+
+// for (let i = 1; i < 10; i++) {
+// console.log(i.toString().repeat(i));
+// }
+
+for (let i = 100; i >= 5; i-5) {
+        console.log(i);
+}
