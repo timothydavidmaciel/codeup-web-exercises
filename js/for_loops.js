82aee46f15036c81e2a268643c3932ddf6a10506
Timothydavid Maciel <timothydavidmaciel@gmail.com>
2022-04-22 14:19:23 -0500

    iterating execerises

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,6 +1,7 @@
+// (() => {
 // function showMultiplicationTable(num) {
 //     for (let i = 1; i < 11; i++) {
-//         console.log(num * i);
+//         console.log(num + " x " + i + " = " + (num * i));
 //     }
 // }
 
@@ -21,26 +22,5 @@
 //         console.log(i);
 // }
 
-// for (let i = 0; i < 100; i++) {
-//     let answer = parseFloat(prompt("Please enter a odd number between 1 and 50."));
-//     if (typeof answer === "number") {
-//         break;
-//     }
-// }
-//
-//
-//
 
-
-
-// for (let i = 1; i < 51; i++) {
-//     if (!needsAnswer === false) {
-//
-//     }
-// }
-
-// for (let i = 1; i < 51; i++) {
-//     if (i % 2 !== 0) {
-//         console.log("Here is an odd number: " + i);
-//     } if ()
-// }
+// })();
