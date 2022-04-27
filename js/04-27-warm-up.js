132e3663c5067260c72a5d20bef6515dbe456dc0
Timothydavid Maciel <timothydavidmaciel@gmail.com>
2022-04-27 10:38:26 -0500

    updating

--- js/04-27-warm-up.js
+++ js/04-27-warm-up.js
@@ -3,8 +3,9 @@
 function allFirstLetters(stt) {
     let bucket = "";
     for (let el of stt) {
-        bucket += el.substring(0, 1)
+       bucket += el.substring(0, 1)
     }
+    return bucket;
 }
 
 const testXStrings = ["dog", "cat", "frog", "bat"];
