55829d3877dc4f86013d6a135c40b978ff4b7878
Timothydavid Maciel <timothydavidmaciel@gmail.com>
2022-05-08 18:37:36 -0500

    pushing Continue and Break execersie

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,4 +1,20 @@
-(() => {
+"use strict";
 
+(function () {
+    let userP;
 
+    while (true) {
+        userP = parseFloat(prompt("Give me an odd number between 1 and 50, please."));
+        if (userP % 2 !== 0 && userP >= 1 && userP <= 50) {
+            break;
+        }
+    }
+
+    for(let j = 1; j <= 50; j += 2) {
+        if (j === userP) {
+            console.log("Sorry, we don't talk about  " + userP);
+            continue;
+        }
+        console.log(j);
+    }
 })();
