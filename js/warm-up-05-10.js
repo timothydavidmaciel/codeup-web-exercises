079e9072c4ccb430da2d37aa315b58b82da2aafa
Timothydavid Maciel <timothydavidmaciel@gmail.com>
2022-05-10 09:19:05 -0500

    Warm Up 05-10

---
+++ js/warm-up-05-10.js
@@ -0,0 +1,45 @@
+const hamsters = [
+    {
+        name: "Hamtaro",
+        heightInMM: 86,
+        fur: ['orange', 'white'],
+        gender: "male",
+        dateOfBirth: "August 6"
+    } , {
+        name: "Bijou",
+        heightInMM: 75,
+        fur: ['white'],
+        gender: "female",
+        dateOfBirth: "July 10"
+    } , {
+        name: "Oxnard",
+        heightInMM: 100,
+        fur: ['grey', 'white'],
+        gender: "male",
+        dateOfBirth: "May 3"
+    } , {
+        name: "Boss",
+        heightInMM: 120,
+        fur: ['brown', 'white'],
+        gender: "male",
+        dateOfBirth: "Spetember 21"
+    } , {
+        name: "Snoozer",
+        heightInMM: 85,
+        fur: ['brown', 'white', "pink"],
+        gender: "male",
+        dateOfBirth: "January 14"
+    }
+];
+
+function whoIsColor(hamsters) {
+    let color = {fur: []};
+    for (let hamster of hamsters) {
+        if(hamster.fur.length > color.fur.length) {
+            color = hamster;
+        }
+    }
+    return color;
+}
+
+console.log(whoIsColor(hamsters))
