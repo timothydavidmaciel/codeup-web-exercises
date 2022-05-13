0ed6893cf6f9920479f86523268275f12b3842fa
Timothydavid Maciel <timothydavidmaciel@gmail.com>
2022-05-13 15:59:50 -0500

    defuseTheBOM

---
+++ js/warm-up-05-11.js
@@ -0,0 +1,14 @@
+firstObject('tacocat') {
+    string: 'tacocat',
+        size: 7.
+    containsLetter: true,
+        isOneWord: true
+}
+
+function makeObjectFromString(str){
+    const =obj = {};
+    obj.string = str;
+    obj.size =str.length;
+    obj.isOneWord =str.indexOf("") <= 0;
+    let lc = str
+}
