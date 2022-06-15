f8079939a0c52fbbb82a2a663a98f6518c308ddf
Timothydavid Maciel <timothydavidmaciel@gmail.com>
2022-06-15 15:21:57 -0500

    updating repo

--- js/weather_map.js
+++ js/weather_map.js
@@ -9,12 +9,35 @@ let map = new mapboxgl.Map({
     zoom: 10// starting zoom
 });
 
+let weatherData = $.get("http://api.openweathermap.org/data/2.5/onecall", {
+    APPID: OPEN_WEATHER_KEY,
+    units: "imperial",
+    lat: 29.4252,
+    lon: -98.4946
+}).then((data) => {
+    let weatherCards = "";
+    for (let i = 0; i < 5; i++) {
+        weatherCards +=
+            `  <div class="card d-flex flex-row" style="width: auto;">
+                  <div class="card-body">
+                    <div class="card-title">${new Date(data.daily[i].dt * 1000)}</div>
+                    <p class="card-text">Temp: ${data.daily[i].temp.day}</p
+                </div>                     
+              </div>
+        `
+    }
+    document.getElementById("5Day").innerHTML = weatherCards;
+})
+
+// Marker Start Point
 const marker = new mapboxgl.Marker({
     draggable: true
 })
     .setLngLat([-98.4946, 29.4252])
     .addTo(map);
 
+
+// When dragging the marker it will update the location with the current forecast in that area
 function onDragEnd() {
     const lngLat = marker.getLngLat();
     let renderWD = $.get("http://api.openweathermap.org/data/2.5/onecall", {
@@ -42,36 +65,23 @@ function onDragEnd() {
 
 marker.on('dragend', onDragEnd);
 
-
-// San Antonio Weather
-
-    // change below
-
-// need to fix below
-
-// let geoData = marker
-// // let geoData = $.get(`http://api.openweathermap.org/geo/1.0/direct?q=San-Antonio,us&limit=5&appid=${OPEN_WEATHER_KEY}`)
-// //     .then((data) => {
-//     let geoInsert = "";
-//     for (let i = 0; i < 1; i++) {
-//         geoInsert +=
-//             `<div>${geoData.lon}, ${geoData.lat}</div>`
-//         // }
-//         document.getElementById("testCity").innerHTML = geoInsert;
-//     }
-
+// Using geocode to update when searched
+document.getElementById("submitSearch").addEventListener("click", function (e) {
+    e.preventDefault();
+let inputValue = document.getElementById("search").value;
+console.log(inputValue)
+geocode(inputValue, MAPBOX_KEY_API).then((res) => {
     let weatherData = $.get("http://api.openweathermap.org/data/2.5/onecall", {
         APPID: OPEN_WEATHER_KEY,
         units: "imperial",
-        lat: 29.4252,
-        lon: -98.4946
-        // lat: 29.4252,
-        // lon: -98.4946
+        lat: `${res[1]}`,
+        lon: `${res[0]}`
     }).then((data) => {
+        console.log(data)
         let weatherCards = "";
         for (let i = 0; i < 5; i++) {
-          weatherCards +=
-              `  <div class="card d-flex flex-row" style="width: auto;">
+            weatherCards +=
+                `  <div class="card d-flex flex-row" style="width: auto;">
                   <div class="card-body">
                     <div class="card-title">${new Date(data.daily[i].dt * 1000)}</div>
                     <p class="card-text">Temp: ${data.daily[i].temp.day}</p
@@ -80,73 +90,11 @@ marker.on('dragend', onDragEnd);
         `
         }
         document.getElementById("5Day").innerHTML = weatherCards;
+        marker.setLngLat(res);
+        map.setCenter(res);
     })
+})
+
+})
 
 
-
-// const geocoder = new MapboxGeocoder({
-//     // Initialize the geocoder
-//     accessToken: mapboxgl.accessToken, // Set the access token
-//     mapboxgl: mapboxgl, // Set the mapbox-gl instance
-//     marker: false // Do not use the default marker style
-// });
-
-const geocoder = new MapboxGeocoder({
-    accessToken: mapboxgl.accessToken,
-    marker: {
-        color: 'orange'
-    },
-    mapboxgl: mapboxgl
-});
-
-// Add the geocoder to the map
-map.addControl(geocoder)
-
-// console.log(geocoder)
-//
-// function searchBox() {
-//     const lngLat = geocoder;
-//     let searchWM = $.get("http://api.openweathermap.org/data/2.5/onecall", {
-//         APPID: OPEN_WEATHER_KEY,
-//         units: "imperial",
-//         lat: lngLat.eventManager.proximity.latitude,
-//         lon: lngLat.eventManager.proximity.longitude
-//     }).done((searchData) => {
-//         let searchCards = "";
-//         for (let i = 0; i < 5; i++) {
-//             searchCards +=
-//                 `  <div class="card d-flex flex-row" style="width: auto;">
-//                   <div class="card-body">
-//                     <div class="card-title">${new Date(searchData.daily[i].dt * 1000)}</div>
-//                     <p class="card-text">Temp: ${searchData.daily[i].temp.day}</p
-//                 </div>
-//               </div>
-//         `
-//         document.getElementById("5Day").innerHTML = searchCards;
-//     }
-// })}
-
-// function searchBox() {
-//     const lngLat = geocoder;
-//     console.log(lngLat.typeahead.selected.center[0])
-//     let searchWM = $.get("http://api.openweathermap.org/data/2.5/onecall", {
-//         APPID: OPEN_WEATHER_KEY,
-//         units: "imperial",
-//         lat: lngLat.typeahead.selected.center[1],
-//         lon: lngLat.typeahead.selected.center[0]
-//     }).done((searchData) => {
-//         console.log();
-//         // let weatherCards = "";
-//         // for (let i = 0; i < 5; i++) {
-//         //     weatherCards +=
-//         //         `  <div class="card d-flex flex-row" style="width: auto;">
-//         //           <div class="card-body">
-//         //             <div class="card-title">${new Date(data.daily[i].dt * 1000)}</div>
-//         //             <p class="card-text">Temp: ${data.daily[i].temp.day}</p
-//         //         </div>
-//         //       </div>
-//         // `
-//     })}
-// document.getElementById("5Day").innerHTML = weatherCards;
-// })
-// }
